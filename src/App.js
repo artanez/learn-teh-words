3d278719809bc9834356d42da43d43ab8241e9b7
Igor Kudryashov <aartanez@gmail.com>
2020-06-22 20:26:01 +0300

    release v1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          hello world, 42!
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
